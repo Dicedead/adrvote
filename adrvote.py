4b01885e4bae269fa4fd6975db15688bfebc9665
dicedead <dicedead@gmail.com>
2024-10-28 23:38:06 +0100

    can find sections of classreps

--- adrvote.py
+++ adrvote.py
@@ -1,8 +1,13 @@
 import requests
+import tqdm
+
+import pandas as pd
+
 from bs4 import BeautifulSoup
 
 
-SECTION_LIST = ["cgc"]
+SECTION_LIST = ["cms", "ar", "cgc", "gc", "gm", "el", "in", "sv", "ma", "mt", "ph", "mx", "sie", "sc", "if", "mte",
+                "nx", "siq", "dh"]
 
 
 def extract_names_from_html(html_content: str):
@@ -25,7 +30,38 @@ def fetch_and_save_text(url: str, filename: str):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching the URL: {e}")
 
+
+def check_string_in_file(file_path, search_string):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            return search_string in content
+    except FileNotFoundError:
+        print(f"The file {file_path} was not found.")
+        return False
+
+
+def update_sections():
+
+    for section in SECTION_LIST:
+        url = f"https://cadiwww.epfl.ch/listes/viewlist?list=etudiants.{section}@epfl.ch"
+        filename = f"res/sectionlists/{section}"
+        fetch_and_save_text(url, filename)
+
+    reps_csv_path = "res/studentreps.csv"
+    reps = pd.read_csv(reps_csv_path)
+
+    reps_names = reps["Name"]
+    reps_sections = [""] * len(reps_names)
+    for i in tqdm.trange(len(reps_names)):
+        for section in SECTION_LIST:
+            if check_string_in_file(f"res/sectionlists/{section}", reps_names[i]):
+                reps_sections[i] = section
+    reps["Section"] = reps_sections
+
+    return reps, reps_sections
+
+
 if __name__ == "__main__":
-    url = "https://cadiwww.epfl.ch/listes/viewlist?list=etudiants.dh@epfl.ch"
-    filename = "res/sectionlists/dh"
-    fetch_and_save_text(url, filename)
+    res, res2 = update_sections()
+    print(res)
